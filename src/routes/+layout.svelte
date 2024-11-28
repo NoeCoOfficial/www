c28a5108b969e3bc364fdf6d62807abb58c7156b
Ingo <me@ingowolf.au>
2024-11-29 07:37:12 +1100

    move nav to layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 </script>
 
-{@render children()}
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset>
+		{@render children()}
+	</Sidebar.Inset>
+</Sidebar.Provider>
