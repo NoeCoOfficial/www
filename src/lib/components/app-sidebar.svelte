5eece8dae1159fe1901939f9513770bf5ddf6707
Ingo <me@ingowolf.au>
2024-11-29 07:24:51 +1100

    clean up `data`

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -10,11 +10,6 @@
 	import SquareTerminal from 'lucide-svelte/icons/square-terminal';
 
 	const data = {
-		user: {
-			name: 'shadcn',
-			email: 'm@example.com',
-			avatar: '/avatars/shadcn.jpg'
-		},
 		navMain: [
 			{
 				title: 'Playground',
@@ -100,35 +95,6 @@
 						url: '#'
 					}
 				]
-			}
-		],
-		navSecondary: [
-			{
-				title: 'Support',
-				url: '#',
-				icon: LifeBuoy
-			},
-			{
-				title: 'Feedback',
-				url: '#',
-				icon: Send
-			}
-		],
-		projects: [
-			{
-				name: 'Design Engineering',
-				url: '#',
-				icon: Frame
-			},
-			{
-				name: 'Sales & Marketing',
-				url: '#',
-				icon: ChartPie
-			},
-			{
-				name: 'Travel',
-				url: '#',
-				icon: Map
 			}
 		]
 	};
