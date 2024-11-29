12224583cb6bc3bc0687308f4d854fa6571a85fc
Ingo <me@ingowolf.au>
2024-11-29 14:48:37 +1100

    fix sizing and add text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,9 +19,20 @@
 		</Breadcrumb.Root>
 	</div>
 </header>
-<div class="m-4 rounded-xl bg-muted/50 p-2">
-	<div class="flex min-h-96 flex-col items-center justify-center gap-8 p-8 md:flex-row">
+<div class="m-4 flex items-center justify-center rounded-xl bg-muted/50 p-2">
+	<div class="flex min-h-96 max-w-prose flex-col items-center justify-center gap-8 p-8 md:flex-row">
 		<enhanced:img src="$lib/images/NoeCoLogoTransparent-Cropped.png" alt="" />
 		<h1 class="text-center text-5xl">Making indie games for everyone</h1>
+	</div>
+</div>
+<div class="flex items-center justify-center">
+	<div class="flex min-w-10 max-w-prose text-xl">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+		voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. aa sadfasfd asd f
+		sa dfa sd faa sadfasfd asd f sa dfa sd faa sadfasfd asd f sa dfa sd faa sadfasfd asd f sa dfa sd
+		f
 	</div>
 </div>
