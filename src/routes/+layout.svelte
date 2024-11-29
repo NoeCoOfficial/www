e98a787a8a6dd0efd09beb862ce9ad2359432ba3
Ingo <me@ingowolf.au>
2024-11-29 13:55:15 +1100

    fix sizing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,8 @@
 <Sidebar.Provider>
 	<AppSidebar />
 	<Sidebar.Inset>
-		{@render children()}
+		<div class="w-full flex-shrink-0 overflow-auto" style="height: calc(100vh - 20px)">
+			{@render children()}
+		</div>
 	</Sidebar.Inset>
 </Sidebar.Provider>
