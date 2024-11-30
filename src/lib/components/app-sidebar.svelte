3fd141ed7597df5f58daf5ba53440c14d466dd4e
Ingo <me@ingowolf.au>
2024-11-30 14:04:15 +1100

    modify sidebar

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -44,7 +44,7 @@
 	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
 </script>
 
-<Sidebar.Root bind:ref variant="inset" collapsible="icon" {...restProps}>
+<Sidebar.Root bind:ref variant="inset" {...restProps}>
 	<Sidebar.Header>
 		<Sidebar.Menu>
 			<Sidebar.MenuItem>
