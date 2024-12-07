6e8b4d0422a1b65964f124426ad601ed18f5705d
Ingo <me@ingowolf.au>
2024-12-07 12:58:19 +1100

    make simple games page

---
+++ src/routes/games/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	import * as Card from '$lib/components/ui/card/index.js';
+</script>
+
+<div class="flex items-center justify-center p-4">
+	<Card.Root class="w-full max-w-prose">
+		<Card.Header>
+			<Card.Title>Expland</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<p>
+				Face adversity, hardship, enjoy adventure as well, and discover a second chance to fix past
+				mistakes and become a better person in a mysterious world with nightmares and happiness just
+				around the corner.
+			</p>
+		</Card.Content>
+		<Card.Footer>
+			<div>
+				<Button href="https://github.com/NoeCoOfficial/Expland/releases" target="_blank" size="lg"
+					>Download</Button
+				>
+				<Button variant="outline" href="/games/expland" size="lg">More</Button>
+			</div>
+		</Card.Footer>
+	</Card.Root>
+</div>
