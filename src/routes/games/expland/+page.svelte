9e75b8555a1d2b686f45b9ea7a62d8e03c2ecee9
Ingo <me@ingowolf.au>
2024-12-07 11:36:40 +1100

    add video background

---
+++ src/routes/games/expland/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import preview from './preview.webm';
+</script>
+
+<video src={preview} autoplay muted loop class="fixed left-0 top-0 h-screen w-screen object-cover"
+></video>
