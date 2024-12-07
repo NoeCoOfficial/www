a3b1bd11c8023f7c5776a91707b5b7e2f53196f1
sebashtioon <sebastiansuciu607@gmail.com>
2024-12-07 22:10:29 +1100

    update team member details on about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -29,7 +29,7 @@
 	</div>
 </header>
 
-<h1 class="text-4xl font-bold text-center my-8">Meet the Team</h1>
+<h1 class="text-6xl font-extrabold text-center my-8 underline">Meet the Team</h1>
 
 <div class="m-4 flex items-center justify-center rounded-xl p-2">
 	<div class="max-w-prose">
@@ -41,32 +41,40 @@
 <div class="m-4 flex flex-col gap-6 items-start justify-center rounded-xl p-4">
 	<div>
 		<h2 class="scroll-m-20 text-2xl font-semibold tracking-tight text-white">
-			Name 1
+			Sebastian Suciu
 		</h2>
 		<p class="text-lg text-gray-300">
-			Name 1 is a passionate game developer who specializes in design and
-			storytelling. They bring the team's ideas to life with innovative mechanics and
-			captivating narratives.
+			Seb is a 14 year old in high school who loves messing around 
+			with computers and programming languages. 
+			His favorite video game is Geometry Dash, 
+			and the engine he uses to make games is Godot and Blender. 
+			He has been making games and projects for over 2 years, and he has been 
+			loving every second of it. He enjoys playing Basketball and sport and is skilled 
+			in GDScript, Python and C++.
 		</p>
 	</div>
 	<div>
 		<h2 class="scroll-m-20 text-2xl font-semibold tracking-tight text-white">
-			Name 2
+			Tristan Taylor
 		</h2>
 		<p class="text-lg text-gray-300">
-			Name 2 focuses on programming and technology, ensuring smooth and efficient
-			performance in every game. Their technical expertise helps the team tackle
-			challenging problems.
+			Tristan is 14 years old and unlike Seb, 
+			enjoys making game assets rather than coding, 
+			as well as being skilled in Blender, 
+			Soundtrap and paperwork. Tristan is our main source of ideas for the team. 
+			He is great at making lore, storyboarding and game planning.
+			His favorite video game is Subnautica.
 		</p>
 	</div>
 	<div>
 		<h2 class="scroll-m-20 text-2xl font-semibold tracking-tight text-white">
-			Name 3
+			Erik Miller
 		</h2>
 		<p class="text-lg text-gray-300">
-			Name 3 is a talented artist and animator, creating visually stunning and
+			Erik is a talented artist and animator, creating visually stunning and
 			immersive game environments. They add the finishing touch to the team's
-			creations.
+			creations. Erik is 14 years old and loves making pixel art and music for 
+			the team. He holds skills in Pixilart and Soundtrap.
 		</p>
 	</div>
 </div>
