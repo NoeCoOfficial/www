f608666bb4d338a2ed477bfb7f01df1bef33a196
Ingo <me@ingowolf.au>
2024-12-07 13:00:15 +1100

    add header

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js';
-
 	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 </script>
 
+<header class="sticky top-0 flex h-16 w-full shrink-0 items-center gap-2 backdrop-blur-xl">
+	<div class="flex items-center gap-2 px-4">
+		<Sidebar.Trigger class="-ml-1" />
+		<Separator orientation="vertical" class="mr-2 h-4" />
+		<Breadcrumb.Root>
+			<Breadcrumb.List>
+				<Breadcrumb.Item>
+					<Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+				</Breadcrumb.Item>
+				<Breadcrumb.Separator></Breadcrumb.Separator>
+				<Breadcrumb.Item>
+					<Breadcrumb.Page>Games</Breadcrumb.Page>
+				</Breadcrumb.Item>
+			</Breadcrumb.List>
+		</Breadcrumb.Root>
+	</div>
+</header>
 <div class="flex items-center justify-center p-4">
 	<Card.Root class="w-full max-w-prose">
 		<Card.Header>
