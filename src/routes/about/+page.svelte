522042d974691a012cf16dcc3d53a3b7ad47cc40
Ingo <me@ingowolf.au>
2024-12-07 14:25:15 +1100

    add about us button

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import SvelteSeo from 'svelte-seo';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 </script>
 
 <SvelteSeo
@@ -7,3 +10,21 @@
 	description="We are a team of three who like making games. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
 		et dolore magna aliqua."
 />
+
+<header class="sticky top-0 flex h-16 w-full shrink-0 items-center gap-2 backdrop-blur-xl">
+	<div class="flex items-center gap-2 px-4">
+		<Sidebar.Trigger class="-ml-1" />
+		<Separator orientation="vertical" class="mr-2 h-4" />
+		<Breadcrumb.Root>
+			<Breadcrumb.List>
+				<Breadcrumb.Item>
+					<Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+				</Breadcrumb.Item>
+				<Breadcrumb.Separator></Breadcrumb.Separator>
+				<Breadcrumb.Item>
+					<Breadcrumb.Page>About Us</Breadcrumb.Page>
+				</Breadcrumb.Item>
+			</Breadcrumb.List>
+		</Breadcrumb.Root>
+	</div>
+</header>
--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
 	import SvelteSeo from 'svelte-seo';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 </script>
 
 <SvelteSeo title="Privacy Policy - Noe Co." />
+
+<header class="sticky top-0 flex h-16 w-full shrink-0 items-center gap-2 backdrop-blur-xl">
+	<div class="flex items-center gap-2 px-4">
+		<Sidebar.Trigger class="-ml-1" />
+		<Separator orientation="vertical" class="mr-2 h-4" />
+		<Breadcrumb.Root>
+			<Breadcrumb.List>
+				<Breadcrumb.Item>
+					<Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+				</Breadcrumb.Item>
+				<Breadcrumb.Separator></Breadcrumb.Separator>
+				<Breadcrumb.Item>
+					<Breadcrumb.Page>Privacy Policy</Breadcrumb.Page>
+				</Breadcrumb.Item>
+			</Breadcrumb.List>
+		</Breadcrumb.Root>
+	</div>
+</header>
