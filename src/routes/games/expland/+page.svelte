faaeb066d37a286edb893cbe822f1a97b1639d3d
Ingo <me@ingowolf.au>
2024-12-07 13:24:10 +1100

    switch to mp4

--- src/routes/games/expland/+page.svelte
+++ src/routes/games/expland/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import preview from './preview.webm';
+	import preview from './preview.mp4';
 	import textlogo_white from './textlogo_white.png';
 
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
