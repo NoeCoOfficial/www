92d8156943ec24f38a9a9a46932a84a7d4e9a69f
Ingo <me@ingowolf.au>
2024-12-08 15:01:47 +1100

    make content better to read

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -41,32 +41,34 @@
 	</div>
 </div>
 
-<div class="m-4 flex flex-col items-start justify-center gap-6 rounded-xl p-4">
-	<div>
-		<h2 class="scroll-m-20 text-2xl font-semibold tracking-tight text-white">Sebastian Suciu</h2>
-		<p class="text-lg text-gray-300">
-			Seb is a 14 year old in high school who loves messing around with computers and programming
-			languages. His favorite video game is Geometry Dash, and the engine he uses to make games is
-			Godot and Blender. He has been making games and projects for over 2 years, and he has been
-			loving every second of it. He enjoys playing Basketball and sport and is skilled in GDScript,
-			Python and C++.
-		</p>
-	</div>
-	<div>
-		<h2 class="scroll-m-20 text-2xl font-semibold tracking-tight text-white">Tristan Taylor</h2>
-		<p class="text-lg text-gray-300">
-			Tristan is 14 years old and unlike Seb, enjoys making game assets rather than coding, as well
-			as being skilled in Blender, Soundtrap and paperwork. Tristan is our main source of ideas for
-			the team. He is great at making lore, storyboarding and game planning. His favorite video game
-			is Subnautica.
-		</p>
-	</div>
-	<div>
-		<h2 class="scroll-m-20 text-2xl font-semibold tracking-tight text-white">Erik Miller</h2>
-		<p class="text-lg text-gray-300">
-			Erik is a talented artist and animator, creating visually stunning and immersive game
-			environments. They add the finishing touch to the team's creations. Erik is 14 years old and
-			loves making pixel art and music for the team. He holds skills in Pixilart and Soundtrap.
-		</p>
+<div class="flex justify-center">
+	<div class="m-4 flex max-w-screen-lg flex-col items-start justify-center gap-6 rounded-xl p-4">
+		<div>
+			<h2 class="scroll-m-20 text-2xl font-semibold tracking-tight text-white">Sebastian Suciu</h2>
+			<p class="text-lg text-gray-300">
+				Seb is a 14 year old in high school who loves messing around with computers and programming
+				languages. His favorite video game is Geometry Dash, and the engine he uses to make games is
+				Godot and Blender. He has been making games and projects for over 2 years, and he has been
+				loving every second of it. He enjoys playing Basketball and sport and is skilled in
+				GDScript, Python and C++.
+			</p>
+		</div>
+		<div>
+			<h2 class="scroll-m-20 text-2xl font-semibold tracking-tight text-white">Tristan Taylor</h2>
+			<p class="text-lg text-gray-300">
+				Tristan is 14 years old and unlike Seb, enjoys making game assets rather than coding, as
+				well as being skilled in Blender, Soundtrap and paperwork. Tristan is our main source of
+				ideas for the team. He is great at making lore, storyboarding and game planning. His
+				favorite video game is Subnautica.
+			</p>
+		</div>
+		<div>
+			<h2 class="scroll-m-20 text-2xl font-semibold tracking-tight text-white">Erik Miller</h2>
+			<p class="text-lg text-gray-300">
+				Erik is a talented artist and animator, creating visually stunning and immersive game
+				environments. They add the finishing touch to the team's creations. Erik is 14 years old and
+				loves making pixel art and music for the team. He holds skills in Pixilart and Soundtrap.
+			</p>
+		</div>
 	</div>
 </div>
