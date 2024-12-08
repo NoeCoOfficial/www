b39b0c284414935c6b4cbba69e56d3b51438dbdc
Ingo <me@ingowolf.au>
2024-12-08 16:55:34 +1100

    show output

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,6 +74,8 @@ export default defineConfig({
 	webServer: {
 		command: 'pnpm dev',
 		url: 'http://127.0.0.1:5173',
-		reuseExistingServer: !process.env.CI
+		reuseExistingServer: !process.env.CI,
+		stdout: 'pipe',
+		stderr: 'pipe'
 	}
 });
