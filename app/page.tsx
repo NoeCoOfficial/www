a5d63ceb6db97b109fc785bd8a6cd43efaac4260
Ingo Wolf <me@ingowolf.au>
2025-08-18 00:29:04 +1000

    add animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,50 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 export default function Home() {
   return (
     <>
       <div className="max-w-2xl w-full grid grid-cols-2  mx-auto p-3 gap-10 mt-20">
-        <Image
-          src="/logo-transparent.png"
-          alt="Noe Co. Logo"
-          className="max-w-52 ml-auto self-center"
-          width={500}
-          height={305}
-        />
-        <h1 className="text-left text-4xl font-bold self-center">
-          Making indie games for everyone.
-        </h1>
+        <motion.div
+          className="self-center"
+          initial={{ opacity: 0, x: -25 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ ease: [0.165, 0.84, 0.44, 1.0] }}
+        >
+          <Image
+            src="/logo-transparent.png"
+            alt="Noe Co. Logo"
+            className="max-w-52 ml-auto"
+            width={500}
+            height={305}
+          />
+        </motion.div>
+        <motion.div
+          className="self-center"
+          initial={{ opacity: 0, x: 25 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ ease: [0.165, 0.84, 0.44, 1.0], delay: 0.1 }}
+        >
+          <h1 className="text-left text-4xl font-bold">
+            Making indie games for everyone.
+          </h1>
+        </motion.div>
       </div>
-      <div className="max-w-2xl w-full mx-auto mt-5">
+      <motion.div
+        className="max-w-2xl w-full mx-auto mt-5"
+        initial={{ opacity: 0, y: 25 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ ease: [0.165, 0.84, 0.44, 1.0], delay: 0.2 }}
+      >
         We are a partnership of two developers looking to find joy in making
         video games. Currently as of writing, we only have one game in
         development, called Expland, which we plan to be our first game to
         release to the public. We are putting our heart and soul into this game,
         and we hope you enjoy it when it comes out. Stay tuned!
-      </div>
+      </motion.div>
     </>
   );
 }
