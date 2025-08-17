e8e70f5ee08b8e344cbae3fa9029ea717a0dd507
Ingo Wolf <me@ingowolf.au>
2025-08-18 00:23:09 +1000

    add pages

---
+++ app/games/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Games",
+};
+
+export default function GamesPage() {
+  return <div></div>;
+}
---
+++ app/privacy/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Privacy",
+};
+
+export default function PrivacyPage() {
+  return <div></div>;
+}
