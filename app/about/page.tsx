823375418a5fff21bec7cd48e63bada2fba37c2a
Ingo Wolf <me@ingowolf.au>
2025-08-20 16:51:47 +1000

    add socials

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,6 +10,13 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faDiscord,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "About",
@@ -30,11 +37,6 @@ export default function AboutPage() {
         <Card>
           <CardHeader>
             <CardTitle>Sebastian Suciu</CardTitle>
-            <CardDescription>
-              <Link href="https://github.com/sebashtioon" target="_blank">
-                @sebashtioon
-              </Link>
-            </CardDescription>
           </CardHeader>
           <CardContent>
             Seb is a 15-year-old high school student who loves messing around
@@ -44,15 +46,38 @@ export default function AboutPage() {
             he has been loving every second of it. He enjoys playing Basketball
             and sport and is skilled in GDScript, Python, and C++.
           </CardContent>
+          <CardFooter>
+            <div className="flex flex-row gap-1 not-prose">
+              <Button variant="ghost" size="icon" asChild>
+                <Link href="https://github.com/sebashtioon" target="_blank">
+                  <span className="sr-only">Github</span>
+                  <FontAwesomeIcon icon={faGithub} />
+                </Link>
+              </Button>
+              <Button variant="ghost" size="icon" asChild>
+                <Link
+                  href="https://discord.com/users/1110329250306859018"
+                  target="_blank"
+                >
+                  <span className="sr-only">Discord</span>
+                  <FontAwesomeIcon icon={faDiscord} />
+                </Link>
+              </Button>
+              <Button variant="ghost" size="icon" asChild>
+                <Link
+                  href="https://www.youtube.com/@sebashtioon_"
+                  target="_blank"
+                >
+                  <span className="sr-only">Youtube</span>
+                  <FontAwesomeIcon icon={faYoutube} />
+                </Link>
+              </Button>
+            </div>
+          </CardFooter>
         </Card>
         <Card>
           <CardHeader>
             <CardTitle>Tristan Taylor</CardTitle>
-            <CardDescription>
-              <Link href="https://github.com/GoatsAreTB" target="_blank">
-                @GoatsAreTB
-              </Link>
-            </CardDescription>
           </CardHeader>
           <CardContent>
             Tristan is 15 years old and, unlike Seb, enjoys making game assets
@@ -61,6 +86,25 @@ export default function AboutPage() {
             making lore, storyboarding, and game planning. His favorite video
             game is Subnautica.
           </CardContent>
+          <CardFooter>
+            <div className="flex flex-row gap-1 not-prose">
+              <Button variant="ghost" size="icon" asChild>
+                <Link href="https://github.com/GoatsAreTB" target="_blank">
+                  <span className="sr-only">Github</span>
+                  <FontAwesomeIcon icon={faGithub} />
+                </Link>
+              </Button>
+              <Button variant="ghost" size="icon" asChild>
+                <Link
+                  href="https://discord.com/users/1188276399048503346"
+                  target="_blank"
+                >
+                  <span className="sr-only">Discord</span>
+                  <FontAwesomeIcon icon={faDiscord} />
+                </Link>
+              </Button>
+            </div>
+          </CardFooter>
         </Card>
       </div>
     </Content>
