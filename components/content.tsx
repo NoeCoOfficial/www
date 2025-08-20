33b8d603b1b65448c55683ba230fb987a153528c
Ingo Wolf <me@ingowolf.au>
2025-08-20 19:52:08 +1000

    add spacing

--- components/content.tsx
+++ components/content.tsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 export default function Content({ children }: { children: React.ReactNode }) {
   return (
     <motion.div
-      className="prose mt-15 mx-auto prose-custom max-w-5xl w-full p-2"
+      className="prose my-15 mx-auto prose-custom max-w-5xl w-full p-2"
       initial={{ opacity: 0, y: 25 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ ease: [0.165, 0.84, 0.44, 1.0], delay: 0.1 }}
