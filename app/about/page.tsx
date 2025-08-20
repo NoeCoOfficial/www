68c54c8c2c72346faa64ee1318a3046f2ab04ebe
Ingo Wolf <me@ingowolf.au>
2025-08-20 19:58:15 +1000

    change some content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,8 +15,7 @@ export default function AboutPage() {
       games. Currently, as of writing, we only have one game in development,
       called Expland, which we plan to be our first game to release to the
       public. We are putting our heart and soul into this game, and we hope you
-      enjoy it when it comes out. Stay tuned! All of our socials are linked
-      below.
+      enjoy it when it comes out. Stay tuned!
       <h2>Meet the Team</h2>
       <Team />
       <h2>Dedications</h2>
