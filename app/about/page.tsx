8b311b5854dfc1e6aa2c06e1f9f567833690de0b
Ingo Wolf <me@ingowolf.au>
2025-08-20 17:04:49 +1000

    add more content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import Content from "@/components/content";
 import { Metadata } from "next";
 import { Team } from "./team";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "About",
@@ -18,6 +19,52 @@ export default function AboutPage() {
       below.
       <h2>Meet the Team</h2>
       <Team />
+      <h2>Dedications</h2>
+      <ul>
+        <li>Mel Timpson, for being the world's greatest school teacher.</li>
+        <li>
+          Erik Miller, another founder of Noe Co., who left to pursue other
+          things.
+        </li>
+        <li>The team at the STEMVGC</li>
+      </ul>
+      <h2>Shout-outs</h2>
+      <ul>
+        <li>Shaheer Usman / Shazman</li>
+        <li>Xavier Hosking / Javier</li>
+        <li>Kai Steele</li>
+        <li>Dylan Wilson / yoyobros</li>
+        <li>Evan Banks</li>
+        <li>amacd_</li>
+        <li>Kepax / Kphlo</li>
+      </ul>
+      <h2>Special Thanks</h2>A heartfelt thank you to the tools and communities
+      that have helped us along the way:
+      <ul>
+        <li>
+          <Link href="https://godotengine.org/" target="_blank">
+            Godot Engine
+          </Link>
+          : For providing an amazing open-source game development platform.
+        </li>
+        <li>
+          <Link href="https://forum.godotengine.org/" target="_blank">
+            Godot Forums
+          </Link>{" "}
+          and{" "}
+          <Link href="https://stackoverflow.com/" target="_blank">
+            Stack Overflow
+          </Link>
+          : For giving us <span className="line-through">code to copy</span>{" "}
+          answers to literally everything.
+        </li>
+        <li>
+          <Link href="https://www.youtube.com/" target="_blank">
+            YouTube
+          </Link>
+          : For providing a free learning resource and millions of tutorials.
+        </li>
+      </ul>
     </Content>
   );
 }
