035f121130f72908b33bd0fcd68749d6929addbf
Ingo Wolf <me@ingowolf.au>
2025-08-20 18:17:37 +1000

    add scroll animations

--- app/games/expland/content.tsx
+++ app/games/expland/content.tsx
@@ -1,12 +1,31 @@
 "use client";
 
 import { ChevronDown } from "lucide-react";
-import { motion } from "motion/react";
+import { motion, useScroll, useTransform } from "motion/react";
 import Image from "next/image";
 import { useRef } from "react";
 
 export function ExplandPageContent() {
+  const heroSection = useRef(null);
   const textSection = useRef(null);
+  const { scrollYProgress: heroSectionScrollYProgress } = useScroll({
+    target: heroSection,
+    offset: ["0%", "50%"],
+  });
+  const { scrollYProgress: textSectionScrollYProgress } = useScroll({
+    target: textSection,
+    offset: ["-50%", "0%"],
+  });
+  const heroSectionOpacity = useTransform(
+    heroSectionScrollYProgress,
+    [0, 1],
+    [1, 0],
+  );
+  const textSectionScale = useTransform(
+    textSectionScrollYProgress,
+    [0, 1],
+    [0.9, 1],
+  );
 
   return (
     <motion.div
@@ -15,7 +34,11 @@ export function ExplandPageContent() {
       transition={{ duration: 2, delay: 0 }}
       className="bg-black"
     >
-      <motion.div className="w-full h-screen -z-10">
+      <motion.div
+        className="w-full h-screen -z-10"
+        ref={heroSection}
+        style={{ opacity: heroSectionOpacity }}
+      >
         <div className="h-[50%] w-full from-black to-transparent bg-gradient-to-t absolute bottom-0 left-0 z-10"></div>
         <div className="h-96 opacity-75 w-full from-black to-transparent bg-gradient-to-b absolute top-0 left-0 z-10"></div>
         <motion.div
@@ -52,11 +75,17 @@ export function ExplandPageContent() {
         className="w-full h-screen flex flex-col items-center justify-center text-2xl text-center p-2"
         ref={textSection}
       >
-        <div className="w-full max-w-lg">
+        <motion.div
+          className="w-full max-w-lg"
+          style={{
+            opacity: textSectionScrollYProgress,
+            scale: textSectionScale,
+          }}
+        >
           Face adversity, hardship, enjoy adventure as well, and discover a
           second chance to fix past mistakes and become a better person in a
           mysterious world with nightmares and happiness just around the corner.
-        </div>
+        </motion.div>
       </motion.div>
     </motion.div>
   );
