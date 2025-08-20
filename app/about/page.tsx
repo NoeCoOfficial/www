71595a9cec79ffc460d5069cb9cd81aa9ebbe1d5
Ingo Wolf <me@ingowolf.au>
2025-08-20 16:46:34 +1000

    move to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,7 +19,12 @@ export default function AboutPage() {
   return (
     <Content>
       <h1>About</h1>
-      We are a partnership of two who like making games.
+      We are a partnership of two developers looking to find joy in making video
+      games. Currently, as of writing, we only have one game in development,
+      called Expland, which we plan to be our first game to release to the
+      public. We are putting our heart and soul into this game, and we hope you
+      enjoy it when it comes out. Stay tuned! All of our socials are linked
+      below.
       <h2>Meet the Team</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
         <Card>
