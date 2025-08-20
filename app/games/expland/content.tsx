b10c140762c626083c7ba2dafdfb4ca2a4b1cb70
Ingo Wolf <me@ingowolf.au>
2025-08-20 17:59:34 +1000

    add logo and content

--- app/games/expland/content.tsx
+++ app/games/expland/content.tsx
@@ -1,27 +1,53 @@
 "use client";
 
+import { ChevronDown } from "lucide-react";
 import { motion } from "motion/react";
+import Image from "next/image";
 
 export function ExplandPageContent() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 2, delay: 0 }}
-      className="w-full h-screen bg-black -z-10"
-    >
-      <div className="h-[50%] w-full from-black to-transparent bg-gradient-to-t absolute bottom-0 left-0 z-10"></div>
-      <div className="h-96 opacity-75 w-full from-black to-transparent bg-gradient-to-b absolute top-0 left-0 z-10"></div>
-      <motion.video
-        initial={{ opacity: 0, filter: "blur(30px)" }}
-        animate={{ opacity: 1, filter: "blur(0)" }}
-        transition={{ duration: 1, delay: 2 }}
-        src="/assets/expland/gameplay.webm"
-        className="w-full h-full object-cover absolute top-0 left-0"
-        autoPlay
-        muted
-        loop
-      />
-    </motion.div>
+    <div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 0 }}
+        className="w-full h-screen bg-black -z-10"
+      >
+        <div className="h-[50%] w-full from-black to-transparent bg-gradient-to-t absolute bottom-0 left-0 z-10"></div>
+        <div className="h-96 opacity-75 w-full from-black to-transparent bg-gradient-to-b absolute top-0 left-0 z-10"></div>
+        <motion.div
+          className="z-20 absolute bottom-10 left-[50%] translate-x-[-50%] flex items-center flex-col"
+          initial={{ opacity: 0, scale: 0.8, y: 30 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{ duration: 1, delay: 2, ease: [0.165, 0.84, 0.44, 1.0] }}
+        >
+          <Image
+            src="/assets/expland/textlogo_white.png"
+            alt="Expland Logo"
+            className="w-96"
+            width={885}
+            height={282}
+          />
+          <ChevronDown />
+        </motion.div>
+        <motion.video
+          initial={{ opacity: 0, filter: "blur(30px)" }}
+          animate={{ opacity: 1, filter: "blur(0)" }}
+          transition={{ duration: 1, delay: 2 }}
+          src="/assets/expland/gameplay.webm"
+          className="w-full h-full object-cover absolute top-0 left-0"
+          autoPlay
+          muted
+          loop
+        />
+      </motion.div>
+      <motion.div className="bg-black w-full h-screen flex flex-col items-center justify-center text-2xl text-center p-2">
+        <div className="w-full max-w-lg">
+          Face adversity, hardship, enjoy adventure as well, and discover a
+          second chance to fix past mistakes and become a better person in a
+          mysterious world with nightmares and happiness just around the corner.
+        </div>
+      </motion.div>
+    </div>
   );
 }
