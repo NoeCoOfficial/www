690279bad01044eb4ed5f3e7ea8b7be9880ee261
Ingo Wolf <me@ingowolf.au>
2025-08-20 14:26:46 +1000

    add about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,5 +5,10 @@ export const metadata: Metadata = {
 };
 
 export default function AboutPage() {
-  return <div></div>;
+  return (
+    <div className="prose mt-5 mx-auto prose-custom max-w-5xl w-full p-2">
+      <h1>About</h1>
+      We are a partnership of two who like making games.
+    </div>
+  );
 }
