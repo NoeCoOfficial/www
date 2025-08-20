38fb713eea4c852794e7cea2f0f39bb05830f9b5
Ingo Wolf <me@ingowolf.au>
2025-08-20 13:46:34 +1000

    add gmaes page

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -5,5 +5,20 @@ export const metadata: Metadata = {
 };
 
 export default function GamesPage() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-2  mx-auto p-2 w-full max-w-5xl md:mt-10 mt-5">
+      <div className="w-full grid md:grid-cols-2 gap-4">
+        <div className="w-full h-full bg-neutral-500 aspect-video rounded"></div>
+        <div className="flex flex-col gap-4">
+          <h2 className="text-4xl">Expland</h2>
+          <p className="text-xl">
+            Face adversity, hardship, enjoy adventure as well, and discover a
+            second chance to fix past mistakes and become a better person in a
+            mysterious world with nightmares and happiness just around the
+            corner.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
