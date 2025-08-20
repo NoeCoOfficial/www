3bdc37b2e4e33be14f610666c92936a5b72af619
Ingo Wolf <me@ingowolf.au>
2025-08-20 16:31:46 +1000

    add better mobile support

--- components/footer.tsx
+++ components/footer.tsx
@@ -27,22 +27,27 @@ export function Footer() {
           </Link>
           .
         </div>
-        <div className="flex flex-row gap-2 justify-end">
+        <div className="flex gap-2 justify-end md:flex-row flex-col">
           <Link href="/about">About</Link>
           <Link href="/privacy">Privacy</Link>
           <Link href="mailto:contact@noeco.xyz">contact@noeco.xyz</Link>
-          <Link href="https://github.com/NoeCoOfficial">
-            <span className="sr-only">Github</span>
-            <FontAwesomeIcon icon={faGithub} />
-          </Link>
-          <Link href="https://discord.gg/QNgcKCAJn3" target="_blank">
-            <span className="sr-only">Discord</span>
-            <FontAwesomeIcon icon={faDiscord} />
-          </Link>
-          <Link href="https://www.youtube.com/@noeco.official" target="_blank">
-            <span className="sr-only">YouTube</span>
-            <FontAwesomeIcon icon={faYoutube} />
-          </Link>
+          <div className="flex flex-row gap-2">
+            <Link href="https://github.com/NoeCoOfficial">
+              <span className="sr-only">Github</span>
+              <FontAwesomeIcon icon={faGithub} />
+            </Link>
+            <Link href="https://discord.gg/QNgcKCAJn3" target="_blank">
+              <span className="sr-only">Discord</span>
+              <FontAwesomeIcon icon={faDiscord} />
+            </Link>
+            <Link
+              href="https://www.youtube.com/@noeco.official"
+              target="_blank"
+            >
+              <span className="sr-only">YouTube</span>
+              <FontAwesomeIcon icon={faYoutube} />
+            </Link>
+          </div>
         </div>
       </div>
     </motion.footer>
