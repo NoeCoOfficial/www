e2f59433bae75bcd8fa0233224448ec41d9e5e83
Ingo Wolf <me@ingowolf.au>
2025-08-21 19:40:53 +1000

    add metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "404",
+  description: "The page you are looking for does not exist.",
+};
 
 export default function NotFound() {
   return (
