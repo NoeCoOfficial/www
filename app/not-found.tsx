3074d9b611a8ba6638f3d4c73fd9c3da08eafec8
Ingo Wolf <me@ingowolf.au>
2025-08-21 19:40:06 +1000

    make home button go home

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
@@ -6,7 +7,9 @@ export default function NotFound() {
       <div className="text-center p-4 gap-2 flex flex-col items-center">
         <h1 className="text-9xl">404</h1>
         <p>The page you are looking for does not exist.</p>
-        <Button>Home</Button>
+        <Button asChild>
+          <Link href="/">Home</Link>
+        </Button>
       </div>
     </div>
   );
