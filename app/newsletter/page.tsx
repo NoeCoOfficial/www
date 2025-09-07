f429afa1bbe22ca5ef3309485a8458e94c7f181d
Ingo Wolf <me@ingowolf.au>
2025-09-07 05:40:04 +0000

    add metadata

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -1,4 +1,10 @@
 import NewsletterBox from "@/components/newsletter";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+    title: "Newsletter"
+}
 
 export default function Newsletter() {
   return (
