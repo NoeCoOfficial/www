68232b832f3ee1f2df4713367e84093c0ace023f
Ingo Wolf <me@ingowolf.au>
2025-09-07 05:39:08 +0000

    add newsletter page

---
+++ app/newsletter/page.tsx
@@ -0,0 +1,9 @@
+import NewsletterBox from "@/components/newsletter";
+
+export default function Newsletter() {
+  return (
+    <div className="my-auto flex flex-col gap-2 p-2 py-12">
+      <NewsletterBox />
+    </div>
+  );
+}
