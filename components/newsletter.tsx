4f782654cc9ed48d0d7ac61ef791e89ffb3a88ec
Ingo Wolf <me@ingowolf.au>
2025-09-07 11:47:12 +1000

    add animations and more props

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -3,6 +3,12 @@
 import clsx from "clsx";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import {
+  motion,
+  TargetAndTransition,
+  Transition,
+  VariantLabels,
+} from "motion/react";
 
 export function NewsletterForm() {
   return (
@@ -19,18 +25,35 @@ export function NewsletterForm() {
   );
 }
 
-export default function NewsletterBox({ className }: { className?: string }) {
+export default function NewsletterBox({
+  className,
+  initial = { opacity: 0, y: 25 },
+  animate = { opacity: 1, y: 0 },
+  transition = { ease: [0.165, 0.84, 0.44, 1.0], delay: 0.2 },
+  heading = "Stay up to date",
+  headingClassName,
+}: {
+  className?: string;
+  initial?: boolean | TargetAndTransition | VariantLabels | undefined;
+  animate?: boolean | TargetAndTransition | VariantLabels | undefined;
+  transition?: Transition<any> | undefined;
+  heading?: string;
+  headingClassName?: string;
+}) {
   return (
-    <div
+    <motion.div
       className={clsx(
         "max-w-prose mx-auto w-full p-4 border border-dashed rounded-md flex flex-col items-center gap-2",
         className,
       )}
+      initial={initial}
+      animate={animate}
+      transition={transition}
     >
-      <h2 className="text-xl font-bold">Stay up to date</h2>
+      <h2 className={clsx("text-xl font-bold", headingClassName)}>{heading}</h2>
       <div className="w-full">
         <NewsletterForm />
       </div>
-    </div>
+    </motion.div>
   );
 }
