d415dc2fd0693e87abc3b9a0c435be04d7b355dc
Ingo Wolf <me@ingowolf.au>
2025-09-07 12:17:03 +1000

    add botid to api/subscribe

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,6 +1,14 @@
 import validator from "validator";
+import { checkBotId } from "botid/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
+  const verification = await checkBotId();
+
+  if (verification.isBot) {
+    return NextResponse.json({ error: "Access denied" }, { status: 403 });
+  }
+
   let email;
   try {
     ({ email } = await request.json());
