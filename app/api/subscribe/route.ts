985afa88d763d20fbd96986d2d764724930122ac
Ingo Wolf <me@ingowolf.au>
2025-12-02 09:48:57 +1100

    remove unused impoprts

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,5 +1,5 @@
 import validator from "validator";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
   let email;
